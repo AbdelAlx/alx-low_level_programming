ef3d78d15137883abc7d702a0ca312f4ab5a2fdd
sahboun <sahboun.abdelmajid@gmail.com>
2023-06-22 06:08:55 -0700

    test that the function positive_or_negative() gives the correct output when given a case of 0.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Test_positive_or_negative - fuction that test the function positive_or_negative() and  gives the correct output when given a case of 0
+ *
+ * return - return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
