e7f753847221ff1f790608db3a767a9d7d86e05c
sahboun <sahboun.abdelmajid@gmail.com>
2023-07-09 06:58:57 -0700

    09/07/2023

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  *
  * return: always return 0
  */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s \n", argv[0]);
+	printf("%s \n", *argv);
 	return (0);
 }
