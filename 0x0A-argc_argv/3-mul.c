d7dfc1974cc762856bffcdd1a2dfd8e4a2c7ccc1
sahboun <sahboun.abdelmajid@gmail.com>
2023-07-10 07:49:26 -0700

    2 numbers multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,38 +3,43 @@
 #include "main.h"
 
 /**
+ * _atoi - char to digit function
+ * @c: char to be converted to digit
+ *
+ * Return: digit
+ */
+int _atoi(char *c)
+{
+	int digit, i, sig;
+
+	i = 0;
+	sig = 0;
+	digit = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] == '-')
+		{
+		sig = 1;
+		i++;
+		}
+
+		digit = digit * 10 + (c[i] - '0');
+		i++;
+	}
+		if (sig == 1)
+		digit = -digit;
+
+		return (digit);
+}
+
+/**
  * main - 2 number multiplication
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
-
-int _atoi (char *c)
-{
-int digit, i, sig;
-
-        i = 0;
-        sig = 0;
-        digit = 0;
-
-        while (c[i] != '\0')
-        {
-                if (c[i] == '-')
-                {
-                        sig = 1;
-                        i++;
-                }
-
-                digit = digit * 10 + (c[i] - '0');
-                i++;
-        }
-        if (sig == 1)
-               digit = - digit;
-
-        return (digit);
-}
-
 int main(int argc, char *argv[])
 {
 	int num1, num2;
