a83a74fa455c2c4d45ac341186a07feb423a88a8
sahboun <sahboun.abdelmajid@gmail.com>
2023-07-10 01:54:50 -0700

    10/07/20023

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,10 +8,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
